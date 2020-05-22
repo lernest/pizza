c11849ec4204c051afb5d55321769c54d0e50601
lernest <19194046+lernest@users.noreply.github.com>
2020-05-22 14:41:00 -0700

    pizza component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Pizza />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pizza from './components/Pizza.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Pizza,
+  },
 }
 </script>
 
